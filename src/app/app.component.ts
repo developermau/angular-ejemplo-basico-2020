4a6efb5771c8f226de7be7de6f6efdd008e4399e
developermau <mauriciorojas.auxiliar@outlook.com>
2020-07-25 13:32:57 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component, VERSION } from "@angular/core";
+import { Component, VERSION } from '@angular/core';
+import { LoginService } from './login.service';
 
-interface Login  {
-  username: string
+interface Model {
+  username: string;
 }
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  model: Login = {
-    username: ""
-  };
+export class AppComponent  {
+  model: Model = {
+    username: ''
+  }; 
+  responseList;
+  
+  constructor(
+    private loginService: LoginService
+  ){}
 
   login(form: any): void {
     this.model = form;
+    const username = this.model['username'];
+    this.loginService.getApi(username)
+    .subscribe(
+      response => (this.responseList = response)
+    );
   }
 }
