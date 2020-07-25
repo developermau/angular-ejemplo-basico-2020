96ac05556d5686fe19bd571477bb601548a10d29
developermau <mauriciorojas.auxiliar@outlook.com>
2020-07-25 12:25:25 -0400

    1st Comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+
+interface Login  {
+  username: string
+}
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  model: Login = {
+    username: ""
+  };
+
+  login(form: any): void {
+    this.model = form;
+  }
 }
